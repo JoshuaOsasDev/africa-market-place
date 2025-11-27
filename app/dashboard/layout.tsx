4e3474c79d3c0b73ac5e94154251e65d8f172f8b
Afunsho Olamide <abolamide77@gmail.com>
2025-11-27 10:19:21 +0100

    Made the orders page responsive

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,20 +1,22 @@
+import { inter } from "../font";
 import NavBar from "./components/Navbar";
 import SideBar from "./components/SideBar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col overflow-hidden bg-[#FAFAFF]">
+    <div className={`${inter.className} flex h-screen flex-col overflow-hidden bg-[#FAFAFF]`}>
       
       <div className=" bg-[#FFFFFF]">
         <NavBar />
       </div>
 
-      
-      <div className="flex w-full grow">
+      <div className="flex w-full grow h-0">
+        
         <div className="hidden h-full flex-none md:block md:w-[264px] bg-[#FFFFFF]">
           <SideBar />
         </div>
-        <div className="grow md:overflow-y-auto px-4 md:px-6 py-4 h-full">
+  
+        <div className="grow md:overflow-y-auto px-4 md:px-[30px] py-4 h-full overflow-y-auto">
           {children}
         </div>
       </div>
