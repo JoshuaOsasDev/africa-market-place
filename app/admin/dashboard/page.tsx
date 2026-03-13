63dc74c6e02520c642155ad102b5255e7a50a199
JoshuaOsasDev <ebighoselejoshua@gmail.com>
2026-03-13 16:33:47 +0000

    fix  home

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -5,7 +5,6 @@ import ProductsAndRegionalPerformance from "@/components/pageComponents/admin/da
 import DashboardSalesPage from "@/components/pageComponents/admin/dashboardSalesPage";
 import DashboardSummary from "@/components/pageComponents/vendorComp/dashboard/dashboardSumarry";
 import { useAdminDashboardAnalytics } from "@/lib/hooks/adminDashboardApi/useAdmin";
-import { useAppSelector } from "@/redux/store";
 
 export default function AdminPage() {
   const { isLoading, adminDashboardAnalytics, adminDashboardAnalyticsError } =
